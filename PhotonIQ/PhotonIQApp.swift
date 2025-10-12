f1bf182d818023e98696ab4e3f77085af17a7584
Mike Dice <mikedice417@hotmail.com>
2025-10-12 08:20:47 -0700

    Initial Commit

---
+++ PhotonIQ/PhotonIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonIQApp.swift
+//  PhotonIQ
+//
+//  Created by Mike Dice on 10/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotonIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
